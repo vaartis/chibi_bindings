3854d94d384b0903d74a6124248132becf77719b
taptap <alexey.e.egorov@gmail.com>
2018-09-29 05:51:47 +0300

    Fix style

--- src/SExp.cpp
+++ src/SExp.cpp
@@ -32,14 +32,14 @@ template <>
 std::optional<std::string> SExp::to() const {
     return sexp_stringp(underlying)
         ? std::make_optional(std::string(sexp_string_data(underlying)))
-        : std::nullopt;;
+        : std::nullopt;
 }
 
 template <>
 std::optional<bool> SExp::to() const {
     return sexp_booleanp(underlying)
         ? std::make_optional(static_cast<bool>(sexp_unbox_boolean(underlying)))
-        : std::nullopt;;
+        : std::nullopt;
 }
 
 SExp::~SExp() {
