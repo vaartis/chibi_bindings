82eed5eef04e862fe0550bbc315af5b345ae1e57
Ekaterina Vaartis <vaartis@cock.li>
2018-10-15 23:17:45 +0300

    Make error handling of registered class things better
    They now return exceptions on type errors

--- src/ClassRegistrator_impl.hpp
+++ src/ClassRegistrator_impl.hpp
@@ -11,7 +11,6 @@ namespace { // Implementation detail, so it's in an anon namespace
             auto member_ptr = static_cast<FieldType Class::**>(sexp_cpointer_value(sexp_opcode_data(self)));
 
             Chibi chibi(context);
-
             return chibi.make_from(this_ptr->**member_ptr);
         }
 
@@ -21,51 +20,73 @@ namespace { // Implementation detail, so it's in an anon namespace
             auto member_ptr = static_cast<FieldType Class::**>(sexp_cpointer_value(sexp_opcode_data(self)));
 
             Chibi chibi(context);
-
-            this_ptr->**member_ptr = convert<FieldType>(chibi, sexp_new_value);
-
-            return SEXP_UNDEF;
+            return convert_or_exception(chibi, self, sexp_new_value, this_ptr->**member_ptr);
         }
 
         template<typename Return, typename Arg1>
-        static sexp call(sexp context, sexp self, long n, sexp this_ptr_sexp, sexp arg1) {
+        static sexp call(sexp context, sexp self, long n, sexp this_ptr_sexp, sexp arg1_sexp) {
             auto this_ptr = static_cast<Class *>(sexp_cpointer_value(this_ptr_sexp));
             auto fnc_ptr = static_cast<Return (Class::**) (Arg1)>(sexp_cpointer_value(sexp_opcode_data(self))); // Extract the function pointer from opcode data
 
             Chibi chibi(context);
 
-            Return res = (this_ptr->**fnc_ptr)(
-                ClassRegistratorHelpers<Class>::convert<Arg1>(chibi, arg1)
-            );
+            Arg1 arg1;
+            if (sexp set_result = convert_or_exception(chibi, self, arg1_sexp, arg1); set_result != SEXP_UNDEF) {
+                return set_result;
+            }
+
+            Return res = (this_ptr->**fnc_ptr)(arg1);
 
             return chibi.make_from(res);
         }
 
         template<typename Return, typename Arg1, typename Arg2>
-        static sexp call(sexp context, sexp self, long n, sexp this_ptr_sexp, sexp arg1, sexp arg2) {
+        static sexp call(sexp context, sexp self, long n, sexp this_ptr_sexp, sexp arg1_sexp, sexp arg2_sexp) {
             auto this_ptr = static_cast<Class *>(sexp_cpointer_value(this_ptr_sexp));
             auto fnc_ptr = static_cast<Return (Class::**) (Arg1, Arg2)>(sexp_cpointer_value(sexp_opcode_data(self)));
 
             Chibi chibi(context);
 
-            Return res = (this_ptr->**fnc_ptr)(
-                ClassRegistratorHelpers<Class>::convert<Arg1>(chibi, arg1),
-                ClassRegistratorHelpers<Class>::convert<Arg2>(chibi, arg2)
-            );
+            Arg1 arg1;
+            if (sexp set_result = convert_or_exception(chibi, self, arg1_sexp, arg1); set_result != SEXP_UNDEF) {
+                return set_result;
+            }
+            Arg2 arg2;
+            if (sexp set_result = convert_or_exception(chibi, self, arg2_sexp, arg2); set_result != SEXP_UNDEF) {
+                return set_result;
+            }
+
+            Return res = (this_ptr->**fnc_ptr)(arg1, arg2);
 
             return chibi.make_from(res);
         }
     private:
-        /** Convert from sexp to the expected value type or throw an exception. */
+        /** Convert from sexp to the expected value type and set to to \a set_result_to or return an exception.
+         *
+         * @arg chibi chibi instance to use
+         * @arg self self pointer for error reporting
+         * @arg convert_from the value we're converting from
+         * @arg set_result_to where the result should go on success
+         *
+         * @returns SEXP_UNDEF if everything went ok or a scheme exception if there was an error
+         */
         template<typename Res>
-        static Res convert(Chibi &chibi, sexp s) {
-            SExp se = chibi.make_SExp(s);
+        static sexp convert_or_exception(Chibi &chibi, sexp self, sexp convert_from, Res &set_result_to) {
+            SExp se = chibi.make_SExp(convert_from);
 
             if (auto r = se.to<Res>(); r.has_value()) {
-                return *r;
+                set_result_to = *r;
             } else {
-                throw std::runtime_error(se.dump_to_string() + " was not of type\n " + typeid(Res).name() + "\nwhen converting");
+                return sexp_xtype_exception(
+                    chibi.context,
+                    self,
+                    (std::string("Expected a value of type ") + typeid(Res).name() + " for convertion").c_str(),
+                    convert_from
+                );
             }
+
+            // Everything is ok, return undef
+            return SEXP_UNDEF;
         }
     };
 }
