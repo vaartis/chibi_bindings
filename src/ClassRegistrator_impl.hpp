e9752714cc7115ffb0faca1423a34ad2c584157a
Ekaterina Vaartis <vaartis@cock.li>
2018-10-20 22:10:13 +0300

    Bump class registrator function argument count

--- src/ClassRegistrator_impl.hpp
+++ src/ClassRegistrator_impl.hpp
@@ -70,6 +70,66 @@ struct ClassRegistratorHelpers {
         return SEXP_UNDEF;
     }
 
+    template <typename Arg1, typename Arg2, typename Arg3>
+    static sexp call_noreturn(sexp context, sexp self, long n, sexp this_ptr_sexp, sexp arg1_sexp, sexp arg2_sexp, sexp arg3_sexp) {
+        auto this_ptr = static_cast<Class *>(sexp_cpointer_value(this_ptr_sexp));
+        auto fnc_ptr = static_cast<void (Class::**) (Arg1, Arg2, Arg3)>(sexp_cpointer_value(sexp_opcode_data(self)));
+
+        Chibi chibi(context);
+
+        std::decay_t<Arg1> arg1;
+        if (sexp set_result = convert_or_exception(chibi, self, arg1_sexp, arg1); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg2> arg2;
+        if (sexp set_result = convert_or_exception(chibi, self, arg2_sexp, arg2); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg3> arg3;
+        if (sexp set_result = convert_or_exception(chibi, self, arg3_sexp, arg3); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        (this_ptr->**fnc_ptr)(arg1, arg2, arg3);
+
+        return SEXP_UNDEF;
+    }
+
+    template <typename Arg1, typename Arg2, typename Arg3, typename Arg4>
+    static sexp call_noreturn(sexp context, sexp self, long n, sexp this_ptr_sexp, sexp arg1_sexp, sexp arg2_sexp, sexp arg3_sexp, sexp arg4_sexp) {
+        auto this_ptr = static_cast<Class *>(sexp_cpointer_value(this_ptr_sexp));
+        auto fnc_ptr = static_cast<void (Class::**) (Arg1, Arg2, Arg3, Arg4)>(sexp_cpointer_value(sexp_opcode_data(self)));
+
+        Chibi chibi(context);
+
+        std::decay_t<Arg1> arg1;
+        if (sexp set_result = convert_or_exception(chibi, self, arg1_sexp, arg1); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg2> arg2;
+        if (sexp set_result = convert_or_exception(chibi, self, arg2_sexp, arg2); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg3> arg3;
+        if (sexp set_result = convert_or_exception(chibi, self, arg3_sexp, arg3); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+
+        std::decay_t<Arg4> arg4;
+        if (sexp set_result = convert_or_exception(chibi, self, arg4_sexp, arg4); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        (this_ptr->**fnc_ptr)(arg1, arg2, arg3, arg4);
+
+        return SEXP_UNDEF;
+    }
+
     // Call helpers
 
     template <typename Return>
@@ -119,6 +179,67 @@ struct ClassRegistratorHelpers {
         }
 
         Return res = (this_ptr->**fnc_ptr)(arg1, arg2);
+
+        return ToSExp<Return>::to(chibi, res);
+    }
+
+    template <typename Return, typename Arg1, typename Arg2, typename Arg3>
+    static sexp call(sexp context, sexp self, long n, sexp this_ptr_sexp, sexp arg1_sexp, sexp arg2_sexp, sexp arg3_sexp) {
+        auto this_ptr = static_cast<Class *>(sexp_cpointer_value(this_ptr_sexp));
+        auto fnc_ptr = static_cast<Return (Class::**) (Arg1, Arg2, Arg3)>(sexp_cpointer_value(sexp_opcode_data(self)));
+
+        Chibi chibi(context);
+
+        std::decay_t<Arg1> arg1;
+        if (sexp set_result = convert_or_exception(chibi, self, arg1_sexp, arg1); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg2> arg2;
+        if (sexp set_result = convert_or_exception(chibi, self, arg2_sexp, arg2); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg3> arg3;
+        if (sexp set_result = convert_or_exception(chibi, self, arg3_sexp, arg3); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+
+        Return res = (this_ptr->**fnc_ptr)(arg1, arg2, arg3);
+
+        return ToSExp<Return>::to(chibi, res);
+    }
+
+    template <typename Return, typename Arg1, typename Arg2, typename Arg3, typename Arg4>
+    static sexp call(sexp context, sexp self, long n, sexp this_ptr_sexp, sexp arg1_sexp, sexp arg2_sexp, sexp arg3_sexp, sexp arg4_sexp) {
+        auto this_ptr = static_cast<Class *>(sexp_cpointer_value(this_ptr_sexp));
+        auto fnc_ptr = static_cast<Return (Class::**) (Arg1, Arg2, Arg3, Arg4)>(sexp_cpointer_value(sexp_opcode_data(self)));
+
+        Chibi chibi(context);
+
+        std::decay_t<Arg1> arg1;
+        if (sexp set_result = convert_or_exception(chibi, self, arg1_sexp, arg1); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg2> arg2;
+        if (sexp set_result = convert_or_exception(chibi, self, arg2_sexp, arg2); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg3> arg3;
+        if (sexp set_result = convert_or_exception(chibi, self, arg3_sexp, arg3); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg4> arg4;
+        if (sexp set_result = convert_or_exception(chibi, self, arg4_sexp, arg4); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+
+        Return res = (this_ptr->**fnc_ptr)(arg1, arg2, arg3, arg4);
 
         return ToSExp<Return>::to(chibi, res);
     }
@@ -176,6 +297,37 @@ struct ClassRegistratorHelpers {
         }
 
         Class *constructed_class = new Class(arg1, arg2);
+
+        auto class_freeing_fun = +[](sexp  ctx, sexp self, long n, sexp ptr_to_free) {
+                                      // Free the pointer we've created
+                                      delete static_cast<Class *>(sexp_cpointer_value(ptr_to_free));
+
+                                      return SEXP_UNDEF;
+                                  };
+
+        return chibi.make_cpointer(constructed_class, class_freeing_fun);
+    }
+
+    template<typename Arg1, typename Arg2, typename Arg3>
+    static sexp construct(sexp context, sexp self, long n, sexp arg1_sexp, sexp arg2_sexp, sexp arg3_sexp) {
+        Chibi chibi(context);
+
+        std::decay_t<Arg1> arg1;
+        if (sexp set_result = convert_or_exception(chibi, self, arg1_sexp, arg1); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg2> arg2;
+        if (sexp set_result = convert_or_exception(chibi, self, arg2_sexp, arg2); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        std::decay_t<Arg3> arg3;
+        if (sexp set_result = convert_or_exception(chibi, self, arg3_sexp, arg3); set_result != SEXP_UNDEF) {
+            return set_result;
+        }
+
+        Class *constructed_class = new Class(arg1, arg2, arg3);
 
         auto class_freeing_fun = +[](sexp  ctx, sexp self, long n, sexp ptr_to_free) {
                                       // Free the pointer we've created
